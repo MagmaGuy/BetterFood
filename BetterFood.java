ff2003e5436201103df9244dd09b71aceb1356cd
MagmaGuy <tiagoarnaut+github@gmail.com>
2019-04-28 03:53:52 +0100

    Version 1.1.0 - Updated to 1.14 - Added message when players eat

--- BetterFood.java
+++ BetterFood.java
@@ -1,6 +1,9 @@
 package com.magmaguy.betterfood;
 
-import org.bukkit.Bukkit;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.apache.commons.lang.WordUtils;
+import org.bstats.Metrics;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,142 +24,118 @@ import java.util.List;
 public class BetterFood extends JavaPlugin implements Listener {
 
     @Override
-    public void onEnable(){
-
-        Bukkit.getLogger().info("BetterFood - Enabled!");
+    public void onEnable() {
+        Metrics metrics = new Metrics(this);
         this.getServer().getPluginManager().registerEvents(this, this);
-
     }
 
     @Override
-    public void onDisable(){
-
-        Bukkit.getLogger().info("BetterFood - Disabled!");
-
+    public void onDisable() {
     }
 
-    List<Material> validFoodMaterialSat2 = new ArrayList<Material>(Arrays.asList(
-            Material.COOKIE
+    List<Material> validFoodMaterialSat1 = new ArrayList<Material>(Arrays.asList(
+            Material.DRIED_KELP
+    ));
 
+    List<Material> validFoodMaterialSat2 = new ArrayList<Material>(Arrays.asList(
+            Material.COOKIE, Material.MELON_SLICE, Material.SWEET_BERRIES
+    ));
+
+    List<Material> validFoodMaterialSat4 = new ArrayList<Material>(Arrays.asList(
+            Material.APPLE
     ));
 
     List<Material> validFoodMaterialSat5 = new ArrayList<Material>(Arrays.asList( //fishes have different data values from cooked_fish
-            Material.BAKED_POTATO, Material.BREAD, Material.COOKED_FISH, Material.COOKED_RABBIT
-
+            Material.BAKED_POTATO, Material.BREAD, Material.COOKED_COD, Material.COOKED_RABBIT
     ));
 
     List<Material> validFoodMaterialSat6 = new ArrayList<Material>(Arrays.asList(
-            Material.BEETROOT_SOUP, Material.COOKED_CHICKEN, Material.COOKED_MUTTON, Material.MUSHROOM_SOUP
-
+            Material.BEETROOT_SOUP, Material.COOKED_CHICKEN, Material.COOKED_MUTTON, Material.COOKED_MUTTON, Material.COOKED_SALMON, Material.MUSHROOM_STEW
     ));
 
     List<Material> validFoodMaterialSat8 = new ArrayList<Material>(Arrays.asList(
-            Material.GRILLED_PORK, Material.PUMPKIN_PIE, Material.COOKED_BEEF
-
+            Material.COOKED_PORKCHOP, Material.PUMPKIN_PIE, Material.COOKED_BEEF
     ));
 
     List<Material> validFoodMaterialSat10 = new ArrayList<Material>(Arrays.asList(
             Material.RABBIT_STEW
-
     ));
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void autoFeed (FoodLevelChangeEvent event) {
+    public void autoFeed(FoodLevelChangeEvent event) {
 
         if (event.getEntity() instanceof Player) {
 
-//            Bukkit.getLogger().info(event.getFoodLevel()+"");
-
             Player player = (Player) event.getEntity();
+
+            if (!player.hasPermission("betterfood.user"))
+                return;
 
             Inventory playerInventory = player.getInventory();
 
-            if (event.getFoodLevel() <= 20 - 4) {
+            if (event.getFoodLevel() <= 20 - 4)
+                if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 4, validFoodMaterialSat1)) {
+                    event.setCancelled(true);
+                    return;
+                }
 
+            if (event.getFoodLevel() <= 20 - 4)
                 if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 4, validFoodMaterialSat2)) {
-
                     event.setCancelled(true);
                     return;
-
                 }
 
-            }
+            if (event.getFoodLevel() <= 20 - 4)
+                if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 4, validFoodMaterialSat4)) {
+                    event.setCancelled(true);
+                    return;
+                }
 
-            if (event.getFoodLevel() <= 20 - 5){
-
+            if (event.getFoodLevel() <= 20 - 5)
                 if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 5, validFoodMaterialSat5)) {
-
                     event.setCancelled(true);
                     return;
-
                 }
 
-            }
-
-            if (event.getFoodLevel() <= 20 - 6){
-
+            if (event.getFoodLevel() <= 20 - 6)
                 if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 6, validFoodMaterialSat6)) {
-
                     event.setCancelled(true);
                     return;
-
                 }
 
-            }
-
-            if (event.getFoodLevel() <= 20 - 8){
-
+            if (event.getFoodLevel() <= 20 - 8)
                 if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 8, validFoodMaterialSat8)) {
-
                     event.setCancelled(true);
                     return;
-
                 }
 
-            }
-
-            if (event.getFoodLevel() <= 20 - 10){
-
+            if (event.getFoodLevel() <= 20 - 10)
                 if (foodLevelIterator(player, playerInventory, event.getFoodLevel(), 10, validFoodMaterialSat10)) {
-
                     event.setCancelled(true);
                     return;
-
                 }
-
-            }
 
         }
 
     }
 
-    private boolean foodLevelIterator (Player player, Inventory playerInventory, int playerFoodLevel, int foodLevelThreshold,
-                                    List<Material> materialList) {
+    private void playerMessage(Player player, String name){
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("[BetterFood] You ate a " + name));
+    }
 
-            for (Material material : materialList) {
+    private boolean foodLevelIterator(Player player, Inventory playerInventory, int playerFoodLevel, int foodLevelThreshold,
+                                      List<Material> materialList) {
 
-                if (playerInventory.contains(material)) {
-
-                    for (ItemStack itemStack : playerInventory) {
-
-                        if (itemStack != null && itemStack.getType().equals(material)) {
-
+        for (Material material : materialList)
+            if (playerInventory.contains(material))
+                for (ItemStack itemStack : playerInventory)
+                    if (itemStack != null && itemStack.getType().equals(material))
+                        if (!itemStack.getItemMeta().hasLore() && !itemStack.getItemMeta().hasDisplayName()) {
                             itemStack.setAmount(itemStack.getAmount() - 1);
                             player.setFoodLevel(playerFoodLevel + foodLevelThreshold);
-//                            int newFoodLevel = playerFoodLevel + foodLevelThreshold;
-//                            int actualFoodLevel = player.getFoodLevel();
-//                            Bukkit.getLogger().info("Result: New food level - " + newFoodLevel + " | actual food level -" + actualFoodLevel);
+                            playerMessage(player, WordUtils.capitalizeFully(itemStack.getType().name().toLowerCase().replace("_", " ")));
                             return true;
-
-
                         }
-
-                    }
-
-
-                }
-
-            }
 
         return false;
 
